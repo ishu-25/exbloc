b0e3c8865e197797acef6e7fe7f2ee404da87b90
ishu-25 <81067584+ishu-25@users.noreply.github.com>
2023-05-15 15:18:34 +0530

    Add files via upload
    example of bloc state

---
+++ product_data.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+
+class ProductData{
+  static List<Map<String, dynamic>> productProducts = [
+    {
+      'id': '1',
+      'name': 'MAC Lipdtick',
+      'description': 'MAC Lipdtick in Red',
+      'price': 20.15,
+      'imageUrl': 'https://sdcdn.io/mac/in/mac_sku_M2LP05_1x1_0.png?width=1080&height=1080',
+    },
+    {
+      'id': '2',
+      'name': 'Eyeshadow palette',
+      'description': 'At least in complementary shades',
+      'price': 24.20,
+      'imageUrl': 'https://cdn.shopify.com/s/files/1/0595/1163/6120/products/FES01_Shade_01_1200x1200.jpg?v=1668152541',
+    },
+    {
+      'id': '3',
+      'name': 'Mascara',
+      'description': 'Applied with a brush or rod',
+      'price': 25.49,
+      'imageUrl': 'https://m.media-amazon.com/images/I/51pQmQhAm0L._SL1000_.jpg',
+    },
+    {
+      'id': '4',
+      'name': 'Highligther',
+      'description': 'Shine your beauty with highligther ',
+      'price': 28.78,
+      'imageUrl': 'https://m.media-amazon.com/images/I/61AD585hJyL.jpg',
+    },
+
+  ];
+
+}
---
+++ wishlist_items.dart
@@ -0,0 +1,3 @@
+import 'package:exbloc2/features/home/models/home_product_data_model.dart';
+
+List<ProductDataModel> wishlistItems = [];
